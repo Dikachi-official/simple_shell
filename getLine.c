415cdf9cb619a787ed1803e82a2f5667d88704b9
Michael Anokwulu <112480000+hercjay@users.noreply.github.com>
2023-02-22 13:18:18 -0800

    Update getLine.c

--- getLine.c
+++ getLine.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * input_buf - buffers chained commands
+ * input_buf - buffers chained commandss
  * @info: parameter struct
  * @buf: address of bufferz
  * @len: address of len var
