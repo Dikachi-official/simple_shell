b4ad552d978fab9d5323de7c454813203f2861b9
Michael Anokwulu <112480000+hercjay@users.noreply.github.com>
2023-02-22 13:15:44 -0800

    Update errors1.c

--- errors1.c
+++ errors1.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * _erratoi - converts a string to an integer
+ * _erratoi - converts a string to an integerz
  * @s: the string to be converted
  * Return: 0 if no numbers in string, converted number otherwise
  *       -1 on error
